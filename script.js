7de61beaeffe764e596cb9a161e93ea4b081d29e
Sohrab momand <sohrabmomand.work@gmail.com>
2024-02-07 09:11:45 +0430

    marker and marker content with form added to the project

--- script.js
+++ script.js
@@ -11,33 +11,53 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 
 if (navigator.geolocation) navigator.geolocation.getCurrentPosition(function (position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
-    console.log(`https://www.google.com/maps/@34.5034423,69.1329456,14.93z?entry=ttu`);
+    console.log(`https://www.google.com/maps/@${latitude}, ${longitude}`);
 
     const coords = [latitude, longitude];
 
-    const map = L.map('map').setView(coords, 20);
+    map = L.map('map').setView(coords, 20);
 
-    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
 
 
-    map.on('click', function (mapEvent) {
-        console.log(mapEvent);
+    map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
 
-        const { lat, lng } = mapEvent.latlng;
-
-        L.marker([lat, lng]).addTo(map)
-            .bindPopup('Workout')
-            .openPopup();
     });
 },
     function () {
         alert("could not get your location")
     }
 );
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    console.log(mapEvent);
+
+    const { lat, lng } = mapEvent.latlng;
+
+    L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+            L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup',
+            }))
+        .setPopupContent('Sohrab')
+        .openPopup();
+})
