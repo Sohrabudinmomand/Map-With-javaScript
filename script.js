5c15de8784233e4a35c0f5aaa1d6694d47e2da33
Sohrab momand <sohrabmomand.work@gmail.com>
2024-02-08 09:05:09 +0430

    the marker that appears while you click on the map is updated

--- script.js
+++ script.js
@@ -13,51 +13,60 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 let map, mapEvent;
 
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-if (navigator.geolocation) navigator.geolocation.getCurrentPosition(function (position) {
-    const { latitude } = position.coords;
-    const { longitude } = position.coords;
-    console.log(`https://www.google.com/maps/@${latitude}, ${longitude}`);
+        const coords = [latitude, longitude];
 
-    const coords = [latitude, longitude];
+        map = L.map('map').setView(coords, 20);
 
-    map = L.map('map').setView(coords, 20);
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+        map.on('click', function (mapE) {
+            mapEvent = mapE;
+            console.log("Clicked event:", mapEvent);
+            form.classList.remove('hidden');
+            inputDistance.focus();
+            L.marker(mapEvent.latlng).addTo(map);
+        });
 
 
-
-    map.on('click', function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus();
-
-    });
-},
-    function () {
-        alert("could not get your location")
-    }
-);
+    },
+        function () {
+            alert("Could not get your location");
+        });
+}
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    console.log(mapEvent);
+    console.log("Form submitted");
+    console.log("mapEvent:", mapEvent);
 
-    const { lat, lng } = mapEvent.latlng;
+    if (mapEvent && mapEvent.latlng) {
+        const { lat, lng } = mapEvent.latlng;
+        console.log("Latitude:", lat, "Longitude:", lng);
 
-    L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-            L.popup({
-                maxWidth: 250,
-                minWidth: 100,
-                autoClose: false,
-                closeOnClick: false,
-                className: 'running-popup',
-            }))
-        .setPopupContent('Sohrab')
-        .openPopup();
-})
+        L.marker([lat, lng])
+            .addTo(map)
+            .bindPopup(
+                L.popup({
+                    maxWidth: 250,
+                    minWidth: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: 'running-popup',
+                })
+            )
+            .setPopupContent('Sohrab')
+            .openPopup();
+    } else {
+        console.error("mapEvent or latlng is undefined");
+    }
+});
+
